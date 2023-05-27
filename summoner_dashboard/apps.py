b7d91efda8e24da4bc00c41c62122326094bbe0c
Diegowh <contacto.diegowh@gmail.com>
2023-05-27 17:11:53 +0200

    Corregidos errores de los html respecto a la sintaxis de Django similar a Jinja2

--- summoner_dashboard/apps.py
+++ summoner_dashboard/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SummonerDataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'summoner_data'
+    name = 'summoner_dashboard'
--- whgg_django/urls.py
+++ whgg_django/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from ..summoner_dashboard import views
+from summoner_dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
